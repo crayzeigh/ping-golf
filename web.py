f4be3ff33cf5022f59ff61b0513c2c90ce722548
Aaron Aldrich <a.aldrich@gmail.com>
2022-09-23 17:54:26 -0400

    better string conversion and rounding

--- web.py
+++ web.py
@@ -10,8 +10,7 @@ def pingback():
     ip_addr = request.remote_addr
     latency = ping(ip_addr, unit='ms') 
 
-    return '<h1> Your ping is: ' + "{:.3f}".format(latency) + 'ms!</h1>'
+    return '<h1> Your ping is: ' + str(round(latency, 3)) + 'ms!</h1>'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
-    
